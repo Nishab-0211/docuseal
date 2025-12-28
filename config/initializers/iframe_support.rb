200d7fe361b3460cc43bcd84efbf437fca5219f8
Nishab-0211 <nisha.b@aviratadefsys.com>
2025-12-28 21:37:05 +0530

    Add iframe support initializer configuration

---
+++ config/initializers/iframe_support.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+Rails.application.config.action_controller.forgery_protection_origin_check = false
+Rails.application.config.action_dispatch.cookies_same_site_protection = :none
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             credentials: true,
+             expose: ['Set-Cookie']
+  end
+end
