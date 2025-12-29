3e88140bcaeed82e231edc273b35a062672a2fbd
Nishab-0211 <nisha.b@aviratadefsys.com>
2025-12-29 09:47:44 +0530

    Update iframe_support.rb

--- app/controllers/concerns/iframe_support.rb
+++ app/controllers/concerns/iframe_support.rb
@@ -11,6 +11,8 @@ module IframeSupport
 
   def iframe_request?
     request.headers['X-Requested-With'] == 'iframe' ||
-      request.referrer&.include?('localhost:5173')
+      request.referrer&.include?('localhost:5173') ||
+      request.referrer&.include?('127.0.0.1:5173') ||
+      request.referrer&.include?('docuseal-railway-production-2b66.up.railway.app')
   end
 end
