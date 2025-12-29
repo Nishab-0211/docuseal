181e4e9aa2eda0ff3d4fa36b84b73cd254637c9b
Nishab-0211 <nisha.b@aviratadefsys.com>
2025-12-29 10:20:44 +0530

    Allow forgery protection in iframe support initializer
    Disabled forgery protection by allowing it in the controller.

--- config/initializers/iframe_support.rb
+++ config/initializers/iframe_support.rb
@@ -3,6 +3,9 @@
 Rails.application.config.action_controller.forgery_protection_origin_check = false
 Rails.application.config.action_dispatch.cookies_same_site_protection = :none
 
+# Add this line if not present:
+Rails.application.config.action_controller.allow_forgery_protection = false
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
