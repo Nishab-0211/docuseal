5ba1d26ffc332527b96f2ca4e5d6faca3cbbb6e0
Nishab-0211 <nisha.b@aviratadefsys.com>
2025-12-29 11:04:25 +0530

    Update iframe_support.rb

--- config/initializers/iframe_support.rb
+++ config/initializers/iframe_support.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
+# Disable CSRF protection for iframe requests
 Rails.application.config.action_controller.forgery_protection_origin_check = false
 Rails.application.config.action_dispatch.cookies_same_site_protection = :none
+Rails.application.config.action_controller.default_protect_from_forgery = false
 
-# Add this line if not present:
-Rails.application.config.action_controller.allow_forgery_protection = false
-
+# CORS configuration
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
